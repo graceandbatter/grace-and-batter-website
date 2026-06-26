6be3e07a40c3c3cd45d5fd39da90bcb33ddd0d2f
Miracle Gardner <hello@graceandbatter.com>
2026-06-26 08:11:31 -0400

    improve mobile layout

--- js/script.js
+++ js/script.js
@@ -5,10 +5,24 @@ const closeMenuButton = document.querySelector(".close-menu");
 // Open menu
 menuButton.addEventListener("click", () => {
 mobileMenu.classList.add("show");
+
+// Prevent page from scrolling
+document.body.style.overflow = "hidden";
 });
 
 // Close menu
 closeMenuButton.addEventListener("click", () => {
 mobileMenu.classList.remove("show");
+
+// Allow page scrolling again
+document.body.style.overflow = "";
+});
+
+// Close menu when any link is clicked
+document.querySelectorAll("#mobile-menu a").forEach(link => {
+link.addEventListener("click", () => {
+mobileMenu.classList.remove("show");
+document.body.style.overflow = "";
+});
 });
 
